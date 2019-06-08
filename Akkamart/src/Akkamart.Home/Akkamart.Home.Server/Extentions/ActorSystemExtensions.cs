06db3198a43c344dffa13d240641902834eaed57
hossein farvashani <hossein@hossein-debian.hossein.debian>
2019-06-08 14:57:06 +0430

    build and published

--- Akkamart/src/Akkamart.Home/Akkamart.Home.Server/Extentions/ActorSystemExtensions.cs
+++ Akkamart/src/Akkamart.Home/Akkamart.Home.Server/Extentions/ActorSystemExtensions.cs
@@ -9,29 +9,27 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Akkamart.Home.Server.Actors.Extentions {
     public static class ActorSystemExtensions {
         public static ActorSystem AddActorsystem (this IServiceCollection services, string confUrl) {
-              var config = ConfigurationLoader.Load (confUrl);
-             var actorSystem = Akka.Actor.ActorSystem.Create("akkamart-system", config);
-             actorSystem.ActorOf(Props.Create(typeof(ClusterListenerActor)), "ClusterListenerActor");
-             var shardProxyRoleName = config.GetString("akka.cluster.singleton-proxy.role");
-             var clientManagerProxy = StartUserClientClusterProxy(actorSystem, shardProxyRoleName);
-             var clientManager = actorSystem.ActorOf(Props.Create(() => new ClientManager()),"client-manager");
-            // var sagaManager = actorSystem.ActorOf(Props.Create(() => new ResourceCreationSagaManager(() => new ResourceCreationSaga())),"resourcecreation-sagamanager");
-            // var resourceStorage = actorSystem.ActorOf(Props.Create(() => new ResourcesStorageHandler()), "resource-storagehandler");
-            // var operationStorage = actorSystem.ActorOf(Props.Create(() => new OperationsStorageHandler()), "operation-storagehandler");
+            var config = ConfigurationLoader.Load (confUrl);
+            var actorSystem = Akka.Actor.ActorSystem.Create ("akkamart-system", config);
 
-            // // Add Actors to DI as ActorRefProvider<T>
-             services.AddAkkatecture(actorSystem)
-                 .AddActorReference<ClientManager>(clientManager );
-            //     .AddActorReference<ResourceCreationSagaManager>(sagaManager)
-            //     .AddActorReference<ResourcesStorageHandler>(resourceStorage)
-            //     .AddActorReference<OperationsStorageHandler>(operationStorage);
-           
+            actorSystem.ActorOf (Props.Create (typeof (ClusterListenerActor)),
+                "ClusterListenerActor");
+
+            var shardProxyRoleName = config.GetString ("akka.cluster.singleton-proxy.role");
+            var clientManagerProxy = StartUserClientClusterProxy (actorSystem,
+                shardProxyRoleName);
+
+            var clientManager = actorSystem.ActorOf (Props.Create (() =>
+                new ClientManager ()), "client-manager");
+
+            services.AddAkkatecture (actorSystem)
+                .AddActorReference<ClientManager> (clientManager);
+
             return actorSystem;
         }
-         public static IActorRef StartUserClientClusterProxy(ActorSystem actorSystem, string proxyRoleName)
-        {
+        public static IActorRef StartUserClientClusterProxy (ActorSystem actorSystem, string proxyRoleName) {
             var clusterProxy = ClusterFactory<ClientManager, ClientActor, ClientId>
-                .StartAggregateClusterProxy(actorSystem, proxyRoleName);
+                .StartAggregateClusterProxy (actorSystem, proxyRoleName);
 
             return clusterProxy;
         }
--- Akkamart/src/Akkamart.Seed1/build.sh
+++ Akkamart/src/Akkamart.Seed1/build.sh
@@ -3,4 +3,5 @@
 
 dotnet restore
 dotnet build
-dotnet publish
+# dotnet publish
+dotnet publish --configuration Release --no-restore --output ./dist /p:PublishWithAspNetCoreTargetManifest="false"
